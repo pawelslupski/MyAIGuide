e9de7bfdaa04fad67ed6dc4be0e7917a3afc5462
pawelslupski <pawelslupski.go@gmail.com>
2026-02-21 14:01:33 +0100

    chore: env variable names update

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 
 import type { Database } from '../db/database.types'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.SUPABASE_URL
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey)
 
